365bdc6487990fa4500a4e1885108c40194430d0
Michelle Lim <18221052@std.stei.itb.ac.id>
2023-12-06 21:42:01 +0900

    fix: overwritten routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import AdminPage from './pages/AdminPage.tsx';
-import BookingData from './pages/BookingData.tsx';
-import LandingPage from './pages/LandingPage.tsx';
-import LoginPage from './pages/LoginPage.tsx';
-import MemberData from './pages/MemberData.tsx';
-import RoomData from './pages/RoomData.tsx';
-import RoomPublic from './pages/RoomPublic.tsx';
-import RoomPublicDetail from './pages/RoomPublicDetail.tsx';
+import AdminPage from "./pages/AdminPage.tsx";
+import BookingData from "./pages/BookingData.tsx";
+import LandingPage from "./pages/LandingPage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import MemberData from "./pages/MemberData.tsx";
+import RoomData from "./pages/RoomData.tsx";
+import RoomPublic from "./pages/RoomPublic.tsx";
+import RoomAdd from "./pages/AddRoom.tsx";
+import RoomDataDetail from "./pages/RoomDataDetail.tsx";
+import RoomPublicDetail from "./pages/RoomPublicDetail.tsx";
 
 function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path='/' element={<LandingPage />} />
-				<Route path='/admin' element={<AdminPage />} />
-				<Route path='/login' element={<LoginPage />} />
-				<Route path='/roompublic' element={<RoomPublic />} />
-				<Route path='/roompublicdetail/:idRoom' element={<RoomPublicDetail />} />
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/admin" element={<AdminPage />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/roompublic" element={<RoomPublic />} />
+				<Route
+					path="/roompublicdetail/:idRoom"
+					element={<RoomPublicDetail />}
+				/>
 
 				{/* admin route */}
-				<Route path='/memberdata' element={<MemberData />} />
-				<Route path='/bookingdata' element={<BookingData />} />
-				<Route path='/roomdata' element={<RoomData />} />
+				<Route path="/memberdata" element={<MemberData />} />
+				<Route path="/bookingdata" element={<BookingData />} />
+				<Route path="/roomdata" element={<RoomData />} />
+				<Route path="/roomdata/add" element={<RoomAdd />} />
+				<Route path="/roomdata/edit/:id" element={<RoomDataDetail />} />
 			</Routes>
 		</Router>
 	);
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 
-import App from './App.tsx';
-import theme from './styles/theme.ts';
+import App from "./App.tsx";
+import theme from "./styles/theme.ts";
+import { ToastContainer } from "react-toastify";
 
-import './index.css';
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "react-toastify/dist/ReactToastify.css";
+ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<ChakraProvider theme={theme}>
+			<ToastContainer />
 			<App />
 		</ChakraProvider>
 	</React.StrictMode>
